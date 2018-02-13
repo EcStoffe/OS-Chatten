036f949e8a8a6fdd6594a7e62e0d1e6369db3d90
Malin Hanak <malinehanak@gmail.com>
2018-02-13 15:16:11 +0100

    lagt till databas, separata js, grundstruktur klar

---
+++ script/reguser.js
@@ -0,0 +1,35 @@
+// Connect to firebase register user
+let userRef = firebase.database().ref('users');
+
+document.getElementById('registerUser').addEventListener('submit', regForm);
+
+//Get values from input
+
+function regForm(e) {
+    e.preventDefault();
+    let username = getInputValue('usrName');
+    let firstname = getInputValue('fName');
+    let lastname = getInputValue('lName');
+    let email = getInputValue('eMail');
+    let password = getInputValue('pssWord');
+
+// Save inputvalue
+    saveInputs(username, firstname, lastname, email, password);
+    $('#newUser').modal('hide');
+}
+
+function getInputValue(id){
+    return document.getElementById(id).value;
+}
+
+function saveInputs(username, firstname, lastname, email, password){
+    let newUserRef = userRef.push();
+    newUserRef.set({
+        userName: username,
+        fName: firstname,
+        lName: lastname,
+        email: email,
+        password: password
+    });
+
+}
