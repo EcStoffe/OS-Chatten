d00e6483f17d29287068b8d6a819d43cc9fa22dd
Malin Hanak <malinehanak@gmail.com>
2018-02-19 22:57:46 +0100

    Just a whole lot of half working things

--- script/login.js
+++ script/login.js
@@ -4,25 +4,28 @@ document.getElementById('chatLogin').addEventListener('submit', acessChat);
 const useremail = document.getElementById('userEmail');
 const password = document.getElementById('userPass');
 
+function acessChat(e) {
+    e.preventDefault();
+
+    const email = useremail.value;
+    const pass = password.value;
+
+    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+
+        window.alert('Error : ' + errorMessage + '<br>' + errorCode);
+
+        // ...
+    });
+} //LOGIN FUNCTION ENDS
+
 //FIREBASE FUNCTION TO CHECK FOR CHANGE IN USER/AUTH CHANGE, IF NOT LOGGED IN VALUE IS NULL
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        // user signed in
-        //document.getElementById('userWelcome').style.display = 'block';
-        //document.getElementById('loginNeeded').style.display = 'none';
         location.href = "chat.html";
     } else {
         // user not signed in
-        document.getElementById('userWelcome').style.display = 'none';
-        document.getElementById('loginNeeded').style.display = 'block';
     }
 }); //firebase.auth().onAuthStateChanged ENDS
-
-
-//CHANGE DISPLAYNAME IN AUTH SERVER CODE
-//TODO: Needs to be connected to input and button.
-/*let user = firebase.auth().currentUser;
-    user.updateProfile({
-        displayName: 'malinH'
-    });*/
-
---
+++ script/write.js
@@ -0,0 +1,41 @@
+//CREATE TIMESTAMP
+let currentDate = new Date();
+let year = currentDate.getFullYear();
+let month = currentDate.getMonth() + 1;
+month = month < 10 ? '0' + month : month;
+let day = currentDate.getDay();
+day = day < 10 ? '0' + day : day;
+let hours = currentDate.getHours();
+hours = hours < 10 ? '0' + hours : hours;
+let minutes = currentDate.getMinutes();
+minutes = minutes < 10 ? '0' + minutes : minutes;
+
+let timeStamp = `${year}-${month}-${day}, ${hours}:${minutes}`;
+
+//TRIGGER SUBMIT EVENTLISTENER
+document.getElementById('chatControls').addEventListener('submit', sendMessage);
+//FUNCTION TO SEND MESSAGES
+function sendMessage(e) {
+    e.preventDefault();
+    let chatText = document.getElementById('message').value;
+    console.log(chatText);
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // Sign in
+            console.log(myUserName);
+            saveMessage(myUserName, chatText, timeStamp);
+            // Clear form
+            document.getElementById('chatControls').reset();
+        }
+    });
+}
+
+function saveMessage(myUserName, chatText, timeStamp){
+    let newMessageRef = {
+        username: myUserName,
+        message: chatText,
+        timestamp: timeStamp
+    };
+
+    firebase.database().ref("chatMessage/").push(newMessageRef)
+}
