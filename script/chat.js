6dd7cef30e16831c5f3d0a62065cd27c4fa583ee
Malin Hanak <malinehanak@gmail.com>
2018-03-01 17:23:45 +0100

    Now finding users that are online and pushing to array

--- script/chat.js
+++ script/chat.js
@@ -8,16 +8,13 @@ function hideUserNav(e) {
 }
 // Firebase auth check for status change
 let isOnline = [];
-let refOn = db.ref('users/');
+//let refOn = db.ref('users/').orderByChild('status').equalTo('Online');
+let refOn = db.ref('users/').child('/');
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         let ref = firebase.database().ref("users/"+user.displayName);
         ref.update({
             status: "Online"
-        }).then(function() {
-            //...
-        }).catch(function() {
-            alert("Something went wrong")
         });
         if(user !== null){
             $('#displayName').html(user.displayName); //display username
@@ -27,14 +24,17 @@ firebase.auth().onAuthStateChanged(function(user) {
             //TODO: Push user into array of objects ((DONE???))
             //TODO: Loop array/object to display
             //TODO: Remove users from array of objects when status update to offline
-
-            refOn.on('child_changed', function(data) {
-                if(data.val().status === "Online") {
-                    isOnline.push({username: data.val().username});
-                    console.log(isOnline)
-                }
-                //console.log('child changed: '+data.val().username, data.val().status);
-                //console.log(isOnline)
+            refOn.on("value", function(data) {
+                let users = data.val();
+                let values = Object.values(users);
+                console.log("users", users);
+                values.forEach(function(onlineUser) {
+                    console.log("username", onlineUser.username+' : '+onlineUser.status);
+                    if(onlineUser.status === "Online"){
+                        isOnline.push({username: onlineUser.username, status: onlineUser.status});
+                    }
+                    console.log(isOnline);
+                });
             });
 
             function logOut(e) {
