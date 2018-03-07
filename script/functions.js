073bdb4bb5fa1b05a84e1f51b03cf24a6ee81c19
Christoffer <jo.christoffer@gmail.com>
2018-03-07 08:50:41 +0100

    copied new code into branch

--- script/functions.js
+++ script/functions.js
@@ -6,6 +6,81 @@
     });
 });*/
 
+// Functions to save user details for registration
+function saveInputs(username, fullname, email, password){
+    firebase.database().ref('users/' + username).set({
+        username: username,
+        name: fullname,
+        email: email,
+        password: password,
+        status: "Online"
+    });
+}
+function regForm(e) {
+    e.preventDefault();
+    let username = $('#newUserName').val();
+    let fullname = $('#newUserFullName').val();
+    let email = $('#newUserEmail').val();
+    let password = $('#newUserPass').val();
+    const promise = firebase.auth().createUserWithEmailAndPassword(email, password);
+    promise.catch(e => alert("Error: "+e.message));
+    firebase.auth().onAuthStateChanged(user => {
+        if(user) {
+            saveInputs(username, fullname, email, password);
+            if(user !== null) {
+                user.updateProfile({ displayName: username })
+                    .then(function() { window.location="chat.html"; })
+                    .catch(function() { }); //...
+            }
+        }
+    });
+}
+// Function to login users
+function enterChat(e) {
+    e.preventDefault();
+    const useremail = $('#userEmail').val();
+    const password = $('#userPass').val();
+    firebase.auth().signInWithEmailAndPassword(useremail, password).catch(function(error) {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+
+        window.alert('Error : ' + errorMessage + '\n' + errorCode);
+    });
+}
+// Function to display or hide user navigation
+function showUserNav(e) {
+    e.preventDefault();
+    $('#userControls').show();
+}
+function hideUserNav(e) {
+    e.preventDefault();
+    $('#userControls').hide();
+}
+// Function to display users that are online.
+function showOnlineUsers(){
+    let existing = $('#onlineWindow');
+    let showOnlineUsers = document.createElement('div');
+    showOnlineUsers.setAttribute('id', 'onlinePresence');
+    $(existing).append(showOnlineUsers);
+    refUsersOnline.on("value", function(data) {
+        let users = data.val();
+        let values = Object.values(users);
+        values.forEach(function(onlineUser) {
+            if(onlineUser.status === "Online"){
+                usersOnline.push({username: onlineUser.username, status: onlineUser.status});
+            }
+        });
+        showOnlineUsers.innerHTML = "";
+        usersOnline.forEach(function (displayUserOnline){
+            let usersDisplay = document.createElement('p');
+            usersDisplay.innerText = displayUserOnline.username;
+            $(showOnlineUsers).append(usersDisplay);
+            usersOnline = [];
+        });
+    });
+}
+
 $('#chatOne').click(function () {
    $('#displayMessageGeneral').css('display', 'block');
     $('#displayMessageRoomOne').css('display', 'none');
@@ -22,3 +97,4 @@ $('#chatThree').click(function () {
     $('#displayMessageRoomOne').css('display', 'none');
     $('#displayMessageRoomTwo').css('display', 'block');
 });
+
--- script/read.js
+++ script/read.js
@@ -1,38 +1,18 @@
 //CALLBACK FUNCTION TO GET DATA
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        //console.log(myUserName);
-        let roomOneref = firebase.database().ref('roomOneMessage/');
-        roomOneref.on('value', getMessage);
+        let messageref = firebase.database().ref('generalMessage/');
+        let existingID = document.getElementById('displayMessage');
+        messageref.on("value", function(data) {
+            let messagesObj = data.val();
+            let messages = Object.values(messagesObj);
+            existingID.innerHTML = "";
 
-        let roomTworef = firebase.database().ref('roomTwoMessage/');
-        roomTworef.on('value', getMessage);
+            messages.forEach(function(message) {
+                let author = message.username;
+                let chatmessage = message.chattext;
+                let timeStamped = message.timestamp;
 
-        let generalref = firebase.database().ref('generalMessage/');
-        generalref.on('value', getMessage);
-
-        function getMessage(data){
-
-            let messageList = document.querySelectorAll('.chatContent');
-            for(let i = 0; i < messageList.length; i++){
-                messageList[i].remove();
-            }
-
-            // TARGET DATA KEYS & VALUES
-            let generalMessage = data.val();
-            let keys = Object.keys(generalMessage);
-
-            //UNCOMMENT THIS TO CONSOLE LOG THE WHOLE USER ARRAY console.log(keys);
-
-            //LOOPS ALL OBJECTS TO RETRIEVE KEY VALUES.
-            for(let i = 0; i < keys.length; i++){
-                let m = keys[i];
-                let author = generalMessage[m].username;
-                let chatmessage = generalMessage[m].chattext;
-                let timeStamped = generalMessage[m].timestamp;
-
-
-                let existingID = document.getElementById('displayMessageGeneral');
                 let mainArticle = document.createElement('article');
                 mainArticle.setAttribute('class', 'chatContent');
                 existingID.appendChild(mainArticle);
@@ -54,9 +34,8 @@ firebase.auth().onAuthStateChanged(function(user) {
                 let ParagraphTwoText = document.createTextNode(chatmessage);
                 paragraphTwo.appendChild(ParagraphTwoText);
 
-                // Scrollbar
-                document.getElementById("displayMessageGeneral").scrollTo(0, 5000);
-            }
-        }
+            });
+            document.getElementById("displayMessage").scrollTo(0, 5000);
+        });
     }
 });
