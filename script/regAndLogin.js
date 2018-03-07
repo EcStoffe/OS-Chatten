09ce4437558a0a4dd214c3cc3fc9990a24b9b8eb
Malin Hanak <malinehanak@gmail.com>
2018-03-07 14:51:48 +0100

    Login bug fixed

--- script/regAndLogin.js
+++ script/regAndLogin.js
@@ -1,6 +1,6 @@
 $('#submitNewUser').on('click', regForm); // Connect to firebase register user
 
-$('#chatLogin').on('click', enterChat); // show user navigation
+$('#chatLogin').on('submit', enterChat); // show user navigation
 firebase.auth().onAuthStateChanged(function(user) { // Firebase check if online state has changed for user.
     if (user) { window.location="chat.html"; }
 });
